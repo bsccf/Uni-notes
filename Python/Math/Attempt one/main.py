bd484a77b32b064fabbc7732e84dc8929f592e4e
Syndicalism <71135237+Syndicalism@users.noreply.github.com>
2021-11-02 16:09:41 +0000

    vault backup: 2021-11-02 16:09:41

--- Python/Math/Attempt one/main.py
+++ Python/Math/Attempt one/main.py
@@ -3,6 +3,23 @@ import os;
 def getPath():
     return os.path.dirname(os.path.dirname(__file__))+"Equations"
 
+class equationFetcher:
+    rootJSON = ""
+    
+    def __init__(this):
+        print("init")
+        this.getRootJson();
+        print( this.rootJSON );
+    
+    def getRootJson(this):
+        rFile = open( 
+            getPath() + "\\Equations.json"
+        )
+        this.rootJSON = rFile.read();
+        rFile.close();
+
+
+
 def count_sub_in_file( filename, s ):
     return open( os.path.dirname(__file__) + "\\" +filename, "r" ).read().count( s );
 
