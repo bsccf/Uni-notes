edfa0f6f622cee80b81c6b187871c934c28c0934
Syndicalism <71135237+Syndicalism@users.noreply.github.com>
2021-11-05 10:17:03 +0000

    vault backup: 2021-11-05 10:17:03

--- Mechanics structures and materials/Materials/7 Strengthening Mechanisms/Stengthening Mechanisms notes.md
+++ Mechanics structures and materials/Materials/7 Strengthening Mechanisms/Stengthening Mechanisms notes.md
@@ -5,7 +5,7 @@ tags: ["NotesPage"]
 
 # Stengthening Mechanisms notes
 
-
+![[yield strength|yield point]]
 
 ![[grain structure control]]
 
---
+++ Python/test/test.py
@@ -0,0 +1,19 @@
+
+print("python do be kinda cringe")
+
+yes = 2
+
+class Test:
+    def __init__( this, gaming ):
+        print("very crnge "+gaming)
+        this.name = "cringe: "+gaming
+    
+    def printName( this ):
+        print(this.name)
+    
+
+print(yes)
+
+stuff = Test("ok "+yes+" python < scratch < java < js < C++")
+
+stuff.printName()
